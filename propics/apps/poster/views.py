a875960a46fa1fc07c992af84d64a92e32319ec4
Andrew Raftery <andrewraftery@gmail.com>
2015-04-04 16:26:42 -0400

    initial commit

---
+++ propics/apps/poster/views.py
@@ -0,0 +1,48 @@
+import cStringIO
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.files.storage import default_storage as storage
+from django.core.files.base import ContentFile
+
+from PIL import Image
+
+from poster import forms
+from poster.utils import general
+
+
+def overlay(request):
+    if request.method == 'POST':
+        form = forms.CropForm(request.POST, request.FILES)
+        if form.is_valid():
+            x = form.cleaned_data.get('x')
+            y = form.cleaned_data.get('y')
+            w = form.cleaned_data.get('w')
+            h = form.cleaned_data.get('h')
+            image = form.cleaned_data.get('image')
+            overlay_choice = form.cleaned_data.get('overlay_choice')
+            image_file = cStringIO.StringIO(image.read())
+            background = Image.open(image_file)
+
+            # sizes we have overlays for
+            sizes = [200, 250, 300, 350, 400, 450, 500, 550, 600]
+
+            size = general.round_down(w, sizes)
+            background = background.convert('RGB').crop((x, y, x + w, y + h)).resize((size, size), Image.ANTIALIAS)
+
+            # overlay
+            foreground = Image.open(storage.open('/overlays/{}_600.png'.format(overlay_choice), 'r')).resize((size, size), Image.ANTIALIAS)
+            background.paste(foreground, (0, 0), foreground)
+
+            output = cStringIO.StringIO()
+            background.save(output, "PNG")
+            content = output.getvalue()
+            output.close()
+            response = HttpResponse(content, content_type='image/png')
+            response['Content-Length'] = len(content)
+            response['Content-Disposition'] = 'filename="propic.png";'
+            return response
+    else:
+        form = forms.CropForm()
+
+    return render(request, 'poster/overlay.html', {'form': form})
